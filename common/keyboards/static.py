e98700ae8403753f5ae97e33dd5c389b1c0014db
CakePeg <mark_bolotov@outlook.com>
2023-08-08 13:00:35 +0500

    Добавлена клавиатура start_menu

---
+++ common/keyboards/static.py
@@ -0,0 +1,7 @@
+from aiogram import types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+###
+start_menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+start_menu.add(types.InlineKeyboardButton(text="Выручка"))
+###
