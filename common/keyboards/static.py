9c5217e7f66f896f7d5d9898b4c7fa0bad66f6ab
CakePeg <mark_bolotov@outlook.com>
2023-08-08 16:11:25 +0500

    Добавление клавиатура для панели управления доступом

--- common/keyboards/static.py
+++ common/keyboards/static.py
@@ -2,8 +2,19 @@ from aiogram import types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 ###
-start_menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
-start_menu.add(types.InlineKeyboardButton(text="Выручка"))
-start_menu.add(types.InlineKeyboardButton(text="План поступления"))
-start_menu.add(types.InlineKeyboardButton(text="Рентабельность"))
+kb_start_menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+kb_start_menu.add(types.InlineKeyboardButton(text="📊 Показатели"))
+kb_start_menu.add(types.InlineKeyboardButton(text="🪪 Управление доступом"))
+###
+
+###
+kb_access_management_menu = types.ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            types.KeyboardButton(text="Добавить"),
+            types.KeyboardButton(text="Удалить")
+        ],
+    ], resize_keyboard=True)
+kb_access_management_menu.add(types.InlineKeyboardButton(text="Список пользователей"))
+kb_access_management_menu.add(types.InlineKeyboardButton(text="↩️ Вернуться в главное меню"))
 ###
