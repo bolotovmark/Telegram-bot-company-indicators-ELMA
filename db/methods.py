e009e22c3040bd206482a6fae6d5581a6831228c
CakePeg <mark_bolotov@outlook.com>
2023-08-08 16:20:10 +0500

    + db_get_list_user

--- db/methods.py
+++ db/methods.py
@@ -15,3 +15,15 @@ async def db_exists_user(user_id):
     except Exception as e:
         print(e)
         return False
+
+
+async def db_get_list_user():
+    try:
+        cur = conn.cursor()
+        rez = cur.execute(f"SELECT * FROM users")
+        user = rez.fetchone()
+        cur.close()
+        return user
+    except Exception as e:
+        print(e)
+        return False
