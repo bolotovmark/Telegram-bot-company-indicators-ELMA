1c4176aae6347f63e2143b6be84508cfd17d2ff9
CakePeg <mark_bolotov@outlook.com>
2023-08-08 16:14:01 +0500

    Добавление состояния для добавления и удаление пользователя

--- common/states/access_states.py
+++ common/states/access_states.py
@@ -7,3 +7,17 @@ class StartMenu(StatesGroup):
 
 class PanelAccessManagement(StatesGroup):
     menu = State()
+
+
+#  PanelAccessManagement
+class FormAddNewUser(StatesGroup):
+    id = State()
+    position_name = State()
+    name = State()
+    position_id = 0
+
+
+#  PanelAccessManagement
+class FormRemoveUser(StatesGroup):
+    id = State()
+    check = State()
