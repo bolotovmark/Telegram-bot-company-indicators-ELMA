959fb1123b283d88a1f88fe7085da7bfe2fca4e2
CakePeg <mark_bolotov@outlook.com>
2023-08-08 11:34:56 +0500

    Метод получения значения выручки обобщен до универсального запроса

--- elma/elma.py
+++ elma/elma.py
@@ -6,6 +6,18 @@ from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
 
 
+class DictWithAttributeAccess(dict):
+    def __getattr__(self, key):
+        return self[key]
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+def parse_response(response):
+    return json.loads(response, object_hook=lambda dictionary: DictWithAttributeAccess(dictionary))
+
+
 class ELMA:
     ELMA_LOGIN = ''
     ELMA_PASSWORD = ''
@@ -34,16 +46,21 @@ class ELMA:
         self.AuthToken = out['AuthToken']
         self.SessionToken = out['SessionToken']
 
-    def elma_get_revenue(self):
-        r = requests.get(
-            'https://elma.eriskip.com/API/REST/Entity/Load?type=7414f89e-b840-4137-8256-3ad2c6213816&id=102',
-            headers={'SessionToken': self.SessionToken,
-                     'AuthToken': self.AuthToken,
-                     'Content-type': 'application/json'})
-
-        print("url:", r.url, "\nstatus code: ", r.status_code)
-        out = json.loads(r.content)
-        print(out)
+    def elma_requests_QueryTree(self, typeuid, eql, select):
+        r = requests.get(f'https://elma.eriskip.com/API/REST/Entity/QueryTree?'
+                         f'type={typeuid}&q={eql}&select={select}',
+                         headers={'SessionToken': self.SessionToken,
+                                  'AuthToken': self.AuthToken,
+                                  'Content-type': 'application/json'})
+        print(r.status_code)
+        if r.status_code != 200:
+            self.auth()
+            return self.elma_requests_QueryTree(typeuid, eql, select)
+        return parse_response(r.content.decode('utf-8'))
 
 
 elma = ELMA()
+out = elma.elma_requests_QueryTree('7414f89e-b840-4137-8256-3ad2c6213816',
+                                   "Name = ’Выручка, без НДС’",
+                                   "Name,Summa,Kommentariy")
+print(out[0].Items[2].Value, out[0].Items[3].Value, out[0].Items[4].Value)
