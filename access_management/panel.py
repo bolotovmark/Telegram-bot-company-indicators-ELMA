e912dd6356069bec4fa9557709aca95b325c323c
CakePeg <mark_bolotov@outlook.com>
2023-08-09 14:05:20 +0500

    fix наименование метода

--- access_management/panel.py
+++ access_management/panel.py
@@ -3,14 +3,14 @@ from common.states.access_states import *
 from common.comands import access_management_menu
 
 
-async def back_to_change_users_panel(message: types.Message):
+async def back_to_access_management_panel(message: types.Message):
     await PanelAccessManagement.menu.set()
     await message.answer('Действие отменено', reply_markup=types.ReplyKeyboardRemove())
     return await access_management_menu(message)
 
 
 def register_handlers_access_management_panel(dp: Dispatcher):
-    dp.register_message_handler(back_to_change_users_panel,
+    dp.register_message_handler(back_to_access_management_panel,
                                 content_types=['text'],
                                 text='↩️ Отменить и вернуться в панель управления',
                                 state=[FormAddNewUser.id, FormAddNewUser.name,
