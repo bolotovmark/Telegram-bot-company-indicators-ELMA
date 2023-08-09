291db00f4b649d87fffe35a1a148d90c05c0682e
CakePeg <mark_bolotov@outlook.com>
2023-08-09 17:14:31 +0500

    fix dynamic_kb_company_indicators

--- common/keyboards/dynamic.py
+++ common/keyboards/dynamic.py
@@ -8,16 +8,20 @@ async def dynamic_kb_company_indicators(offset):
 
     inline_kb_full = InlineKeyboardMarkup(row_width=2)
     if company_indicators:
-        for row in company_indicators[offset:][:5]:
+        offset_list = company_indicators[offset:][:5]
+        for row in offset_list:
             inline_kb_full.add(InlineKeyboardButton(f"{row}", callback_data=f"{row}"))
 
         if offset >= 5:
-            inline_kb_full.row(InlineKeyboardButton("⏪Назад", callback_data="back"),
-                               InlineKeyboardButton("⏩Вперед", callback_data="next"))
+            if len(offset_list) != 5:
+                inline_kb_full.add(InlineKeyboardButton("⏪Назад", callback_data="back"))
+            else:
+                inline_kb_full.row(InlineKeyboardButton("⏪Назад", callback_data="back"),
+                                   InlineKeyboardButton("⏩Вперед", callback_data="next"))
         else:
             inline_kb_full.add(InlineKeyboardButton("⏩Вперед", callback_data="next"))
+
     else:
         inline_kb_full.add(InlineKeyboardButton("⏪Назад", callback_data="back"))
 
-    inline_kb_full.add(InlineKeyboardButton("↩️ Вернуться в панель управления", callback_data="back_to_menu"))
     return inline_kb_full
